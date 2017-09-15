8e467ece3e1fbcf6431f319bfc826020e5df6c18
Jefferson Heard <jheard@teamworks.com>
2017-09-15 09:55:21 -0400

    fail reasonably when a repo isnt found or the filesystem is readonly (common CI environment fixes)

--- onaji/logger.py
+++ onaji/logger.py
@@ -12,26 +12,32 @@ class LoggerPlugin(object):
     def __init__(self, serializer=None):
         self._path = os.getenv('REPO_HOME', '.')
         self._home = os.path.join(self._path, ".onaji")
-
+        self._null = False
+        
         if not os.path.exists(self._home):
             os.mkdir(self._home)
-
-        repo = open_repo(self._path)
-        branch = repo.refs.read_ref('HEAD').decode('utf-8').rsplit('/', 1)[-1]
-        self._commit = open_repo(self._path).head()
-        self._file = open(os.path.join(self._home, branch + '.' + self._commit.decode('utf-8') + ".csv"), 'w')
-        self._writer = csv.writer(self._file)
-        self._testname = None
-        if serializer:
-            self._serializer = serializer
-        else:
-            self._serializer = lambda x: quote(repr(x))
-
+                
+        try:        
+            repo = open_repo(self._path)
+            branch = repo.refs.read_ref('HEAD').decode('utf-8').rsplit('/', 1)[-1]
+            self._commit = open_repo(self._path).head()
+            self._file = open(os.path.join(self._home, branch + '.' + self._commit.decode('utf-8') + ".csv"), 'w')
+            self._writer = csv.writer(self._file)
+            self._testname = None
+            if serializer:
+                self._serializer = serializer
+            else:
+                self._serializer = lambda x: quote(repr(x))
+        except:
+            sys.stderr.write("No repository found or error creating output file. Going into null logging mode.\n")
+            self._null = True
+            
     def pytest_runtest_setup(self, item):
         self._testname = item.name
 
     def pytest_unconfigure(self, config):
-        self._file.close()
+        if not self._null:
+            self._file.close()
 
     def log(self, key, *values):
         """
@@ -42,8 +48,9 @@ class LoggerPlugin(object):
             values: 
 
         """
-        for v in values:
-            self._writer.writerow([self._testname, quote(key), self._serializer(v)])
+        if not self._null:
+            for v in values:
+                self._writer.writerow([self._testname, quote(key), self._serializer(v)])
 
 
 def pytest_configure(config):
